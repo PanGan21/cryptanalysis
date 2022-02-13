004b4dbf053f3eb2a7039871caa9f71062f4844f
PanGan21 <ganelis.pan@gmail.com>
2022-02-13 13:34:58 +0100

    fix index of coincidence

--- 1-basics/monoalphabetic/index-of-coincidence/main.py
+++ 1-basics/monoalphabetic/index-of-coincidence/main.py
@@ -2,22 +2,27 @@ import collections
 
 cipher_text = "SOTHATCIPHERTEXTLOOKSLIKETHIS"
 
-# remove all non alpha and whitespace and force uppercase
-# SOTHATCIPHERTEXTLOOKSLIKETHIS
-cipher_flat = "".join(
-    [x.upper() for x in cipher_text.split()
-     if x.isalpha()]
-)
 
-# Tag em
-N = len(cipher_flat)
-freqs = collections.Counter(cipher_flat)
-alphabet = map(chr, range(ord('A'), ord('Z')+1))
-freqsum = 0.0
+def main():
+    # remove all non alpha and whitespace and force uppercase
+    # SOTHATCIPHERTEXTLOOKSLIKETHIS
+    cipher_flat = "".join(
+        [x.upper() for x in cipher_text.split()
+         if x.isalpha()]
+    )
 
-# Do the math
-for letter in alphabet:
-    freqsum += freqs[letter] * (freqs[letter] - 1)
+    # Tag em
+    N = len(cipher_flat)
+    freqs = collections.Counter(cipher_flat)
+    alphabet = map(chr, range(ord('A'), ord('Z')+1))
+    freqsum = 0.0
 
-IC = freqsum / (N * (N - 1))
-print("%.3f" % IC, "({})".format(IC))
+    # Do the math
+    for letter in alphabet:
+        freqsum += freqs[letter] * (freqs[letter] - 1)
+
+    IC = freqsum / (N * (N - 1))
+    print("%.3f" % IC, "({})".format(IC))
+
+
+main()
